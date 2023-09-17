20c0c1988fb3d079c7fcea99e5bfd2b4c0915b7a
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2023-09-17 12:24:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+// QUESTION #01
+
+
+for (var i = 0; i < 10; i++) {
+ 
+  }
+  
+
+// QUESTION #02
+
+
+for (var i = 0; i <= 11; i++) {
+
+  }
+  
+
+// QUESTION #03
+
+
+for (var i = 0; i <= 4; i++);
+
+  
+
+
+// QUESTION #04
+
+
+for (var j = 0; j < 100; j++) {
+
+  }
+  
+
+// QUESTION #05
+
+for (var i = 3; i > 0; i--) {
+ 
+  }
+  
+
+// QUESTION #06
+
+var arrayLength = yourArray.length;
+
+
+// QUESTION #07
+
+var flag =false; 
+
+
+
+// QUESTION #08
+
+for (var i = 0; i < pets.length; i++) {
+
+  }
+  
+
+// QUESTION #09
+
+for (var i = 0; i < 10; i++) {
+    if (i === 1) {
+      alert(i);
+      break;
+    }
+  }
+  
+
+
+// QUESTION #10
+
+var userNames = ["username1", "username2", "username3"]; array
+var firstName = prompt("Enter first name");
+
+for (var i = 0; i < userNames.length; i++) {
+  if (firstName === userNames[i]) {
+    alert("Enter");
+    break;
+  } else {
+    alert("Please write correct user name");
+    break; 
+  }
+}
+
+
+// QUESTION #11
+
+var matchFound = false;
+for (var i = 0; i < list.length; i++) {
+  if (userInput === list[i]) {
+    alert("Match found");
+    matchFound = true;
+    break;
+  }
+}
+if (!matchFound) {
+  alert("Match not found");
+}
+
+
+// QUESTION #12
+
+var firstArr = ["a", "b", "c", "d", "e", "f"];
+var secondArr = [1, 2, 3, 4, 5, 6];
+
+for (var i = 0; i < firstArr.length; i++) {
+  for (var j = 0; j < secondArr.length; j++) {
+    console.log(firstArr[i] + secondArr[j]);
+  }
+}
+
+
+
+
+
+
+
